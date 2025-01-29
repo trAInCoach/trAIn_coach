203267d50ab45dee6e36ff79ccca9a2910427596
trAInCoach <trainingcryptocoin@gmail.com>
2025-01-29 16:26:40 +0100

    Create test_ai.py

---
+++ tests/test_ai.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestAIModel(unittest.TestCase):
+    def test_basic_functionality(self):
+        self.assertEqual(1+1, 2)  # placeholder test
+
+if __name__ == '__main__':
+    unittest.main()
